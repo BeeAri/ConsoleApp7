553a69c208a6c95d7b4f056e292ffc206b7be4fe
zilan.gunduz <zilan.gunduz@101-14>
2023-01-06 19:49:07 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("c:\\");
+            var dirList = di.GetDirectories();
+            foreach (var folder in dirList)
+            {
+                Console.WriteLine(folder.Name);
+                Don(folder);
+            }
+
+        }
+
+        static void Don(DirectoryInfo di)
+        {
+            try
+            {
+                var dirList = di.GetDirectories();
+                foreach (var folder in dirList)
+                {
+                    Console.WriteLine(folder.Name);
+                    Don(folder);
+                }
+
+            }
+            catch (Exception)
+            {
+
+              
+            }
+            
+
+        }
+    }
+}
